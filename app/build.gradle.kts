cf5f8331a7a8f27c924c55b0dd1cb29f88b5ff01
Freetime Maker <jamieachatz@gmail.com>
2026-01-31 19:22:18 +0100

    Changed a Lot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    
+    implementation(libs.core.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
